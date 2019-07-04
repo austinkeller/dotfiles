49e2ea5fb7c95b444b6761b2b3f796617098bb6f
Austin Keller <austin.keller.ic@smartsheet.com>
2019-07-03 18:02:55 -0700

    remove wsl files now that daily driver is manjaro

--- scripts/init_dotfiles.sh
+++ scripts/init_dotfiles.sh
@@ -5,4 +5,4 @@ ln -s .dotfiles/.inputrc
 cp .dotfiles/.ssh/rc .ssh/rc
 cat .dotfiles/.ssh/config >> .ssh/config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cat .dotfiles/.bashrc_components/wsl_colors >> .bashrc
+
