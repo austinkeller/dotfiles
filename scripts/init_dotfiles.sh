741012fe4517be38ff835410d6c1e29ef0b3a926
Austin Keller <austin.keller.ic@smartsheet.com>
2019-07-03 18:03:39 -0700

    remove .inputrc, which isn't used by zsh

--- scripts/init_dotfiles.sh
+++ scripts/init_dotfiles.sh
@@ -1,7 +1,6 @@
 ln -s .dotfiles/.gitconfig
 ln -s .dotfiles/.tmux.conf
 ln -s .dotfiles/.vimrc
-ln -s .dotfiles/.inputrc
 cp .dotfiles/.ssh/rc .ssh/rc
 cat .dotfiles/.ssh/config >> .ssh/config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
