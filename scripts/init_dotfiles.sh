0fd0fa4ed5e6ff9841e5613e65c6c980453d5638
Austin Keller <austin.keller.ic@smartsheet.com>
2019-07-05 13:31:54 -0700

    automatically configure zsh with zprezto

--- scripts/init_dotfiles.sh
+++ scripts/init_dotfiles.sh
@@ -1,7 +1,14 @@
 ln -s .dotfiles/.gitconfig
 ln -s .dotfiles/.tmux.conf
 ln -s .dotfiles/.vimrc
+ln -s .dotfiles/.zshrc
 cp .dotfiles/.ssh/rc .ssh/rc
 cat .dotfiles/.ssh/config >> .ssh/config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone git@github.com:austinkeller/prezto "${ZDOTDIR:-$HOME}"/.zprezto
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^(README.md|zshrc)(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
