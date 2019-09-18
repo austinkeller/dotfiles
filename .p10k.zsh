36569d7b5d45ca8fd65487f01f1911305d48faa0
Austin Keller <austin.keller.ic@smartsheet.com>
2019-09-18 10:27:55 -0700

    Add AWS profile to status bar

--- .p10k.zsh
+++ .p10k.zsh
@@ -54,6 +54,7 @@ fi
       status                  # exit code of the last command
       command_execution_time  # duration of the last command
       background_jobs         # presence of background jobs
+      aws                     # the current AWS profile, if active
       virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
       anaconda                # conda environment (https://conda.io/)
       pyenv                   # python environment (https://github.com/pyenv/pyenv)
