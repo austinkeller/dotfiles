b430c182c8b5ae2564419b4346e99fcb19afedb3
Austin Keller <austin.keller@smartsheet.com>
2020-01-27 16:20:32 -0800

    Add upstream for zprezto in init script

--- scripts/init_dotfiles.sh
+++ scripts/init_dotfiles.sh
@@ -9,6 +9,9 @@ ln -s .dotfiles/.zshrc
 ln -s .dotfiles/.p10k.zsh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone --recursive git@github.com:austinkeller/prezto "${ZDOTDIR:-$HOME}"/.zprezto
+cd "${ZDOTDIR:-$HOME}"/.zprezto
+git remote add upstream https://github.com/sorin-ionescu/prezto.git
+cd $HOME
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^(README.md|zshrc)(.N); do
