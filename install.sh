6c33efaf7e5b6302359aced02f2ee7fd00abe27c
Austin Keller <austin474@gmail.com>
2026-04-14 16:06:51 -0700

    Stop configuring SSH commit signing

--- install.sh
+++ install.sh
@@ -6,12 +6,6 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 ./scripts/init_dotfiles.sh
 
 if [ -z "$CODESPACES" ]; then
-    # Only set gpg signing if we aren't in a Codespace
-    git config --global user.signingkey "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMMvmx1RhpgLTuLjbp3PHpRahBHs1RE1nl2pOaFhohAZ austinkeller@Austins-MacBook-Pro.local github"
-    git config --global commit.gpgsign true
-    git config --global gpg.format ssh
-    git config --global gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers
-
     echo ""
     echo "Dotfiles installed. To complete setup:"
     echo ""
